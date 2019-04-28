ec18ef4f68816af065ca1d472bf93ef3428a2bab
soichisumi <soichi.sumi@gmail.com>
2019-04-28 22:01:35 +0900

    add app codes

---
+++ app/server.go
@@ -0,0 +1,17 @@
+package app
+
+import (
+	"context"
+	"github.com/soichisumi-sandbox/grpc-custom-error-sample/app/proto"
+)
+
+func NewServer() Server {return Server{}}
+
+type Server struct{}
+
+func (s Server) GetData(ctx context.Context, req *proto.GetDataRequest) (*proto.GetDataResponse, error) {
+	return &proto.GetDataResponse{
+		Str: "result",
+		Num: 5,
+	}, nil
+}
